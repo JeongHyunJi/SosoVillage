d9940fbc7af991f0a4f8d8ffac72c95b91a746b2
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2022-03-09 23:06:01 +0900

    Update CameraSetting

--- RPGgame/Assets/Scripts/FishingGame/fishingButton.cs
+++ RPGgame/Assets/Scripts/FishingGame/fishingButton.cs
@@ -7,14 +7,15 @@ using UnityEngine.EventSystems;
 
 public class fishingButton : MonoBehaviour
 {
-    public bool buttonControll = false; // false면 찌가 멈춤
+    public bool buttonControll = true; // false면 찌가 멈춤
     public GameObject button;
     
     public void stopButtonController()
     {
-        GameObject ThisButton = EventSystem.current.currentSelectedGameObject;
-        button = GameObject.FindGameObjectsWithTag("GameController")[Pond.currentLevel - 1];
-        if (ThisButton == button)
+        GameObject ThisButton = EventSystem.current.currentSelectedGameObject; //방금 선택한 오브젝트(버튼)
+        button = GameObject.FindGameObjectsWithTag("GameController")[Pond.currentLevel - 1]; //원하는 오브젝트
+        Debug.Log(ThisButton + " " + button);
+        if (ThisButton == button) // 이름이 같은지 확인
             stopFloat();
     }
 
