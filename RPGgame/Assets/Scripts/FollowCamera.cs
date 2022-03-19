e41a29653d802d92fc1627adb93f5c4287137201
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2022-03-19 17:14:16 +0900

    Update Forest

--- RPGgame/Assets/Scripts/FollowCamera.cs
+++ RPGgame/Assets/Scripts/FollowCamera.cs
@@ -7,9 +7,6 @@ public class FollowCamera : MonoBehaviour
     public Transform target;
     public Vector3 offset;
 
-    private void Start()
-    {
-    }
     void Update()
     {
         transform.position = target.position + offset;
