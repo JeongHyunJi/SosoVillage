dacbc5c869101f6d03c95eb5ef38fb8f9b0d7a55
Kim Min Jeong <dotoriido@gmail.com>
2022-03-28 09:03:16 +0900

    Player order layer & Add time in Home

--- RPGgame/Assets/Scripts/Home/ScoreCount.cs
+++ RPGgame/Assets/Scripts/Home/ScoreCount.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class ScoreCount : MonoBehaviour
 {
     public static int Score = 0;
     public Text scoreText;
     public GameObject button;
+
+    DateTime StartTime = DateTime.Now;
+    //DateTime EndDate = StartDate.AddDays(+1);
+    //TimeSpan timeCal = EndDate - StartDate;
+    //int timeCalMinute = timeCal.Minutes;
+    //PlayerPrefs.SetString("stTime_1",StartTime.ToString());
+    //PlayerPrefs.SetString("stTime_1",StartTime.)
+    //string timeStr = PlayerPrefs.GetString("stTime_1");
+    //DateTime CurrentTime = DateTime.Now;
+    //TimeSpan timeDif = currentTime - startTime;
+    
 
     void Update()
     {
@@ -18,6 +30,7 @@ public class ScoreCount : MonoBehaviour
         else if (0 < Score && Score < 6)
         {
             scoreText.text = Score + " Time";
+            //scoreText.text = timeDif;
         }
         else
         {
