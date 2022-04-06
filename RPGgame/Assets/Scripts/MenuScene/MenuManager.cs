79c3609aff8d3809fde343c510da55a0a8fdffe1
minji1160 <minji010611@gmail.com>
2022-04-06 22:14:51 +0900

    menuedit3

---
+++ RPGgame/Assets/Scripts/MenuScene/MenuManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+    //public GameObject StatusText;
+    //public GameObject MapText;
+    //public GameObject PausedText;
+
+    public GameObject Paused;
+    //public GameObject ContinueText;
+    //public GameObject QuitText;
+
+    public GameObject Main;
+
+    public GameObject Map;
+    //public GameObject HomeText;
+    //public GameObject ForestText;
+    //public GameObject StoreText;
+
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        Paused.SetActive(false);
+        Map.SetActive(false);
+
+    }
+
+    public void openPaused()
+    {
+        Paused.SetActive(true);
+        Map.SetActive(false);
+        Main.SetActive(false);
+    }
+    public void openMain()
+    {
+        Paused.SetActive(false);
+        Map.SetActive(false);
+        Main.SetActive(true);
+    }
+    public void openMap()
+    {
+        Paused.SetActive(false);
+        Map.SetActive(true);
+        Main.SetActive(false);
+    }
+}
