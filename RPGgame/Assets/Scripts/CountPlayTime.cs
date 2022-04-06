112e1b989d76de4351600b4742917f17dcdaf6c6
Kim Min Jeong <dotoriido@gmail.com>
2022-04-07 01:56:59 +0900

    Fix CountPlayTime
    공통된 시간으로 사용하는 것 성공함.
    But Home에 들어가면 다시 초기화되어 카운트되는 문제 존재함!
    
    식물 다 자라면 초기화하는 방법 함께 고민할 필요 있음.

--- RPGgame/Assets/Scripts/CountPlayTime.cs
+++ RPGgame/Assets/Scripts/CountPlayTime.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System;
 
 public class CountPlayTime : MonoBehaviour
 {
     public Text textTimer;
 
+    //처음 시작 시간을 카운트 할 수 있도록
+    public string check = "None";
+
     void Start()
     {
         //set start time
-        DateTime startTime = DateTime.Now;
-        PlayerPrefs.SetString("stTime_1", startTime.ToString());
+        if (SceneManager.GetActiveScene().name == "Home")
+        {
+            if (check != "Complete")
+            {
+                DateTime startTime = DateTime.Now;
+                PlayerPrefs.SetString("stTime_1", startTime.ToString());
+                check = "Complete";
+            }
+        }
     }
     void Update()
     {
