479edf95737dd93dcc7296737bbcc158ae7c3a66
minji1160 <minji010611@gmail.com>
2022-04-07 18:45:26 +0900

    menuedit4

--- RPGgame/Assets/Scripts/MenuController.cs
+++ RPGgame/Assets/Scripts/MenuController.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Debug.Log("MenuController start");
+    }
+    public void clickMenu()
+    {
+        Debug.Log("MenuController click");
+        SceneManager.LoadScene("MenuScene");
     }
 }
