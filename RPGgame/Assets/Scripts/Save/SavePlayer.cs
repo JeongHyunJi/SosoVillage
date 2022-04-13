e1eb781deab026755395db8777523bf17bb0f058
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2022-04-13 21:36:05 +0900

    Fixed error

--- RPGgame/Assets/Scripts/Save/SavePlayer.cs
+++ RPGgame/Assets/Scripts/Save/SavePlayer.cs
@@ -5,46 +5,46 @@ using UnityEngine;
 
 public class SavePlayer : MonoBehaviour
 {
-    private string playerName;
-    private int coin;
-    private int[] inventory = new int[5];
-    private int[] times = new int[5];
+    private static string playerName; //매 씬마다 값이 초기화 되는 것을 방지하기 위해 static으로 변수 선언
+    private static int coin;
+    private static int[] inventory = new int[5];
+    private static int[] times = new int[5];
 
     private bool IsSave;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        IsSave = PlayerPrefs.HasKey("saved_year");
-
-        if (!IsSave)
+        IsSave = PlayerPrefs.HasKey("saved_name");
+        if (TimeController.isStart) //  한번만 실행되도록 if문 안에 코드 작성
         {
-            Debug.Log("저장된 데이터가 없습니다.");
-            playerName = "guest"; //나중에 입력받도록 수정
-            PlayerPrefs.SetString("saved_name", playerName);
-            coin = 5;
-            inventory = new int[] { 1, 1, 1, 1, 1 };
-            times = new int[] { 2022, 4, 10, 0, 0 }; //yyyy,mm,dd,hh,mm
-            Debug.Log(playerName);
-        }
-        else
-        {
-            Debug.Log("저장된 데이터가 있습니다.");
-            playerName = PlayerPrefs.GetString("saved_name");
-            coin = PlayerPrefs.GetInt("saved_coin");
-            inventory[0] = PlayerPrefs.GetInt("saved_1"); //inventory의 내용물의 개수를 저장
-            inventory[1] = PlayerPrefs.GetInt("saved_2");
-            inventory[2] = PlayerPrefs.GetInt("saved_3");
-            inventory[3] = PlayerPrefs.GetInt("saved_4");
-            inventory[4] = PlayerPrefs.GetInt("saved_5");
-            times[0] = PlayerPrefs.GetInt("saved_year");
-            times[1] = PlayerPrefs.GetInt("saved_month");
-            times[2] = PlayerPrefs.GetInt("saved_day");
-            times[3] = PlayerPrefs.GetInt("saved_hour");
-            times[4] = PlayerPrefs.GetInt("saved_minite");
+            if (!IsSave)
+            {
+                Debug.Log("저장된 데이터가 없습니다.");
+                playerName = "guest"; //나중에 입력받도록 수정
+                coin = 5;
+                inventory = new int[] { 1, 1, 1, 1, 1 };
+                times = new int[] { 1900, 1, 1, 9, 0 }; //yyyy,mm,dd,hh,mm -> 초기세팅 : 1900/1/1/ am 9:00 
+                Debug.Log(coin);
+            }
+            else
+            {
+                Debug.Log("저장된 데이터가 있습니다.");
+                playerName = PlayerPrefs.GetString("saved_name");
+                coin = PlayerPrefs.GetInt("saved_coin");
+                inventory[0] = PlayerPrefs.GetInt("saved_1"); //inventory의 내용물의 개수를 저장
+                inventory[1] = PlayerPrefs.GetInt("saved_2");
+                inventory[2] = PlayerPrefs.GetInt("saved_3");
+                inventory[3] = PlayerPrefs.GetInt("saved_4");
+                inventory[4] = PlayerPrefs.GetInt("saved_5");
+                times[0] = PlayerPrefs.GetInt("saved_year");
+                times[1] = PlayerPrefs.GetInt("saved_month");
+                times[2] = PlayerPrefs.GetInt("saved_day");
+                times[3] = PlayerPrefs.GetInt("saved_hour");
+                times[4] = PlayerPrefs.GetInt("saved_minite");
 
+            }
         }
     }
-
     // Update is called once per frame
     public string GetName()
     {
@@ -63,19 +63,12 @@ public class SavePlayer : MonoBehaviour
 
     public int ReturnCoins()
     {
+        Debug.Log(coin);
         return coin;
     }
 
     public DateTime ReturnTime()
     {
-        Debug.Log(playerName);
-        Debug.Log("실행됨");
-        Debug.Log("times[0]" + times[0]);
-        Debug.Log("times[1]" + times[1]);
-        Debug.Log("times[2]" + times[2]);
-        Debug.Log("times[3]" + times[3]);
-        Debug.Log("times[4]" + times[4]);
-
         DateTime tmpTime = new DateTime(times[0], times[1], times[2], times[3], times[4], 0);
         return tmpTime;
     }
