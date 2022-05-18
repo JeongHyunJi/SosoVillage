d1a4f902f784f02595073cd47893412b45aa24df
minjeong <dotoriido@gmail.com>
2022-05-18 15:50:54 +0900

    Fix Inventory in Menu

--- RPGgame/Assets/Scripts/MenuScene/MenuManager.cs
+++ RPGgame/Assets/Scripts/MenuScene/MenuManager.cs
@@ -5,30 +5,14 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
-[System.Serializable]
-public class InvArray //행에 해당되는 이름
-{
-    public GameObject[] Inv;
-    //Inv[0]=
-    //public Text inventory1;
-    //public Text inventory2;
-    //public Text inventory3;
-    //public Text inventory4;
-    //public Text inventory5;
-    //public Text inventory6;//물고기s
-    //public Text inventory7;//물고기m
-    //public Text inventory8;//물고기l
-}
-
 public class MenuManager : MonoBehaviour
 {
-    public InvArray[] InvList;
-
     public GameObject Paused;
     public GameObject Main;
     public GameObject Map;
     public GameObject SaveCheckPanel;
     public GameObject QuitCheckPanel;
+    private GameObject[] MainInv; //inventory들
     // public Texture2D cursorImg;
 
     public Text playerName;
@@ -40,7 +24,6 @@ public class MenuManager : MonoBehaviour
     int[] inventory = new int[8];
     int coin;
     int heart;
-    // Start is called before the first frame update
 
     private void Start()
     {
@@ -53,20 +36,11 @@ public class MenuManager : MonoBehaviour
         heart = savePlayer.GetComponent<SavePlayer>().ReturnHeart();
         hearts.text = heart + " EA";
         inventory = savePlayer.GetComponent<SavePlayer>().ReturnInvent();
-        //GameObject[] tx = GameObject.FindObjectsOfType<MenuManager>();
-        //print(tx[0]);
-        //for(int i=0; i<8; i++)
-        //{
-        //    InvArray.Inv[i].text= "x " + inventory[0];
-        //}
-        //inventory1.text = "x " + inventory[0];
-        //inventory2.text = "x " + inventory[1];
-        //inventory3.text = "x " + inventory[2];
-        //inventory4.text = "x " + inventory[3];
-        //inventory5.text = "x " + inventory[4];
-        //inventory6.text = "x " + inventory[5];
-        //inventory7.text = "x " + inventory[6];
-        //inventory8.text = "x " + inventory[7];
+        MainInv = GameObject.FindGameObjectsWithTag("MenuInv");
+        for (int i = 0; i < 8; i++)
+        {
+            MainInv[i].GetComponent<UnityEngine.UI.Text>().text = "x " + inventory[i];
+        }
         Paused.SetActive(false);
         Map.SetActive(false);
         SaveCheckPanel.SetActive(false);
